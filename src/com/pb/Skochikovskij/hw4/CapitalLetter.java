9ec6cae07683fedf85786a5176523241b3f5b881
Artem Skochikovskij@ <skochskoch@gmail.com>
2021-10-27 01:24:57 +0300

    init commit

--- src/com/pb/Skochikovskij/hw4/CapitalLetter.java
+++ src/com/pb/Skochikovskij/hw4/CapitalLetter.java
@@ -5,22 +5,25 @@ import java.util.Scanner;
 
 public class CapitalLetter {
 
-    static void newPhrase () {
-        System.out.println("Придумайте любое предложение на Ваш выбор - покажем магию");
+    static  char[] toMass (String makePhrase) {
+        char[] makeMass = makePhrase.toCharArray();
+        return makeMass;
     }
 
+
     public static void main (String[]args){
-        Scanner input = new Scanner(System.in);
-        newPhrase();
-        String myPhrase = input.nextLine();
-        char[] myPhraseMassive = myPhrase.toCharArray();
-        myPhraseMassive[0] = Character.toUpperCase(myPhraseMassive[0]);
-        for (int i = 0; i < myPhraseMassive.length - 1; i++) {
-            if (myPhraseMassive[i] == ' ') {
-                myPhraseMassive[i + 1] = Character.toUpperCase(myPhraseMassive[i + 1]);
+        Scanner input = new Scanner(System.in);                                       // объявляем название сканнер переменной - инпут тут
+        System.out.println("Введите, пожалуйста любую фразу, будем делать мэджик: "); //Пишем что хотим от пользователя
+        String myPhrase = input.nextLine();                                       // Создаем переменную моя фраз - которая считывается с клавиатуры
+        char[] myPhraseMassive = toMass(myPhrase);                                    // Создаём Массив который делается из строки введённой пользователем через метод выше
+        myPhraseMassive[0] = Character.toUpperCase(myPhraseMassive[0]);          //для первой буковки применяем метод - сделать большую буковку
+        for (int i = 0; i < myPhraseMassive.length - 1; i++) {                    //делаем это пока массив не закончится
+            if (myPhraseMassive[i] == ' ')                                            //если на каком-то этапе элемент массива - пробел
+            {
+                myPhraseMassive[i + 1] = Character.toUpperCase(myPhraseMassive[i + 1]);//делаем шаг вперёд и снова пробуем делать следующую букву большой
             }
         }
-        String myPhrase2 = new String(myPhraseMassive);
-        System.out.println(myPhraseMassive);
+        System.out.println(myPhraseMassive);                                    //Вывод на экран
     }
 }
+
---
+++ src/com/pb/Skochikovskij/test/test.java
@@ -0,0 +1,27 @@
+package com.pb.Skochikovskij.test;
+import java.util.Scanner;
+
+public class test {
+
+static  char[] toMass (String makePhrase) {
+    char[] makeMass = makePhrase.toCharArray();
+    return makeMass;
+    }
+
+
+public static void main (String[]args){
+    Scanner input = new Scanner(System.in);                                       // объявляем название сканнер переменной - инпут тут
+    System.out.println("Введите, пожалуйста любую фразу, будем делать мэджик: "); //Пишем что хотим от пользователя
+        String myPhrase = input.nextLine();                                       // Создаем переменную моя фраз - которая считывается с клавиатуры
+    char[] myPhraseMassive = toMass(myPhrase);                                    // Создаём Массив который делается из строки введённой пользователем через метод выше
+         myPhraseMassive[0] = Character.toUpperCase(myPhraseMassive[0]);          //для первой буковки применяем метод - сделать большую буковку
+        for (int i = 0; i < myPhraseMassive.length - 1; i++) {                    //делаем это пока массив не закончится
+        if (myPhraseMassive[i] == ' ')                                            //если на каком-то этапе элемент массива - пробел
+        {
+            myPhraseMassive[i + 1] = Character.toUpperCase(myPhraseMassive[i + 1]);//делаем шаг вперёд и снова пробуем делать следующую букву большой
+        }
+        }
+            System.out.println(myPhraseMassive);                                    //Вывод на экран
+        }
+}
+
